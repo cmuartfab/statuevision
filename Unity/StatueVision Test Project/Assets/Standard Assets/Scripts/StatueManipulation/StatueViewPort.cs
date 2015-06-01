b8abc4dd89489572055741c03e2ed91bf1bdf80b
Ralph <ralph.kim93@gmail.com>
2015-06-01 13:55:42 -0400

    One handed leap control
    Left and right side distinguished. Started on modular statue
    manipulation design.

---
+++ Unity/StatueVision Test Project/Assets/Standard Assets/Scripts/StatueManipulation/StatueViewPort.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatueViewPort : MonoBehaviour {
+	
+	private GameObject[] statues;
+	private GameObject statueClone;
+
+	public void Start() 
+	{
+		//connect all statue scripts with this script
+		statues = GameObject.FindGameObjectsWithTag ("Statue");
+		foreach (GameObject statue in statues) 
+		{
+			statue.GetComponent<StatueScript>().SetStatueViewPort(this);
+		}
+	}
+
+	public void SelectStatue(GameObject statue) 
+	{
+		print ("Select");
+		statueClone = Instantiate (statue, Vector3.zero, Quaternion.identity) as GameObject;
+		statueClone.GetComponent<StatueScript> ().SetIsManipulable (true);
+	}
+
+	public void DeselectStatue()
+	{
+		print ("Deselect");
+		GameObject.Destroy (statueClone);
+	}
+
+}
