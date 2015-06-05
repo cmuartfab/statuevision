5b329455c26ac5138805d8d71edee72dacc669a6
clairesophi <clairehentschker@gmail.com>
2015-06-05 10:25:36 -0400

    StatuevisionGlasgowEnvironmentOne
    sample environment for Glasgow models

---
+++ Unity/StatueVisionTestProject/Assets/Screenshot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Screenshot : MonoBehaviour {
+
+	// Use this for initialization
+	private int pic = 0;
+	
+	void Awake(){
+		DontDestroyOnLoad (this.gameObject);
+	}
+	
+	void Update () {
+		if (Input.GetKey(KeyCode.S)){
+			Application.CaptureScreenshot("Users/clairehentschker/Documents/Development/statuevision/Unity/videoFrameOutput/ss_"+pic.ToString() + ".png");
+			pic += 1;
+		}
+	}
+}
